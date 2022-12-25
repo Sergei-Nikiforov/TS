b195e5c44f7917aece39c4a27b568813289ac045
Sergei-Nikiforov <pclp@inbox.ru>
2022-12-25 22:51:59 +0300

    Using Generic Types

--- generic-types/beginning-of-chapter/.dist/index.js
+++ generic-types/beginning-of-chapter/.dist/index.js
@@ -1,29 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
-let people = [new dataTypes_1.Person("Bob Smith", "London"), new dataTypes_1.Person("Dora Peters", "New York")];
-let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
-let cities = [new dataTypes_1.City('London', 8136000), new dataTypes_1.City('Paris', 2141000)];
-let employees = [new dataTypes_1.Employee('Bob Smith', 'Sales'), new dataTypes_1.Employee('Alice Jones', 'Sales')];
-class DataCollection {
-    constructor(initialItems) {
+class ArrayCollection {
+    constructor() {
         this.items = [];
-        this.items.push(...initialItems);
     }
-    filter(predicate) {
-        return this.items.filter(item => predicate(item));
+    add(...newItems) {
+        this.items.push(...newItems);
     }
-    static reverse(items) {
-        return items.reverse();
+    get count() {
+        return this.items.length;
     }
 }
-let mixedData = new DataCollection([...people, ...products]);
-function isProduct(target) {
-    return target instanceof dataTypes_1.Product;
+class ProductCollection extends ArrayCollection {
+    get(searchTerm) {
+        return this.items.find(item => item.name === searchTerm);
+    }
 }
-let filteredProducts = mixedData.filter(isProduct);
-filteredProducts.forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
-let reversedCities = DataCollection.reverse(cities);
-reversedCities.forEach(c => console.log(`City: ${c.name}, ${c.population}`));
-let peopleCities = DataCollection.reverse(people);
-peopleCities.forEach(p => console.log(`Person: ${p.name}, ${p.city}`));
+class PersonCollection extends ArrayCollection {
+    get(searchTerm) {
+        return this.items.find(item => item.name === searchTerm || item.city === searchTerm);
+    }
+}
+let peopleCollection = new PersonCollection();
+peopleCollection.add(new dataTypes_1.Person("Bob Smith", "London"), new dataTypes_1.Person("Dora Peters", "New York"));
+let productCollection = new ProductCollection();
+productCollection.add(new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25));
+[peopleCollection, productCollection].forEach(c => console.log(`Size: ${c.count}`));
--- generic-types/beginning-of-chapter/src/index.ts
+++ generic-types/beginning-of-chapter/src/index.ts
@@ -1,36 +1,43 @@
 import { City, Person, Product, Employee } from "./dataTypes";
 
-let people = [new Person("Bob Smith", "London"), new Person("Dora Peters", "New York")];
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-let cities = [new City('London', 8136000), new City('Paris', 2141000)];
-let employees = [new Employee('Bob Smith', 'Sales'), new Employee('Alice Jones', 'Sales')];
+type shapeType = { name: string };
 
-class DataCollection<T> {
+interface Collection<T extends shapeType> {
+
+    add(...newItems: T[]): void;
+    get(name: string): T;
+    count: number;
+}
+
+abstract class ArrayCollection<T extends shapeType> implements Collection<T> {
     protected items: T[] = [];
 
-    constructor(initialItems: T[]) {
-        this.items.push(...initialItems);
+    add(...newItems: T[]): void {
+        this.items.push(...newItems);
     }
 
-    filter<V extends T>(predicate: (target: T) => target is V): V[] {
-        return this.items.filter(item => predicate(item)) as V[];
-    }
+    abstract get(searchTerm: string): T;
 
-    static reverse<ArrayType>(items: ArrayType[]): ArrayType[] {
-        return items.reverse();
+    get count(): number {
+        return this.items.length;
     }
 }
 
-let mixedData = new DataCollection<Person | Product>([...people, ...products]);
-function isProduct(target: Person | Product): target is Product {
-    return target instanceof Product;
+class ProductCollection extends ArrayCollection<Product> {
+    get(searchTerm: string): Product {
+        return this.items.find(item => item.name === searchTerm) as Product;
+    }
 }
 
-let filteredProducts = mixedData.filter<Product>(isProduct);
-filteredProducts.forEach(p => console.log(`Product: ${ p.name}, ${p.price}`));
+class PersonCollection extends ArrayCollection<Person> {
+    get(searchTerm: string): Person {
+        return this.items.find(item => item.name === searchTerm || item.city === searchTerm) as Person;
+    }
+}
 
-let reversedCities = DataCollection.reverse<City>(cities);
-reversedCities.forEach(c => console.log(`City: ${c.name}, ${c.population}`));
+let peopleCollection: Collection<Person> = new PersonCollection();
+peopleCollection.add(new Person("Bob Smith", "London"), new Person("Dora Peters", "New York"));
 
-let peopleCities = DataCollection.reverse<Person>(people);
-peopleCities.forEach(p => console.log(`Person: ${p.name}, ${p.city}`));
+let productCollection: Collection<Product> = new ProductCollection();
+productCollection.add(new Product("Running Shoes", 100), new Product("Hat", 25));
+[peopleCollection, productCollection].forEach(c => console.log(`Size: ${c.count}`));

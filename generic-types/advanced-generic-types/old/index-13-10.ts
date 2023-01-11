74c441e96505daab48b7a6df004b29a10236e750
Sergei-Nikiforov <pclp@inbox.ru>
2023-01-11 22:57:57 +0300

    advanced generic types

--- generic-types/advanced-generic-types/old/index-13-10.ts
+++ generic-types/advanced-generic-types/old/index-13-10.ts
@@ -1,12 +1,12 @@
-import { City, Person, Product, Employee } from "./dataTypes";
+import { City, Person, Product, Employee } from "../src/dataTypes";
 
 function getValue<T, K extends keyof T>(item: T, keyname: K) {
     console.log(`Value: ${item[keyname]}`);
 }
 
 let p = new Product("Running Shoes", 100);
-getValue(p, "name");
-getValue(p, "price");
+getValue<Product, 'name'>(p, "name");
+getValue<Product, 'price'>(p, "price");
 
 let e = new Employee("Bob Smith", "Sales");
 getValue(e, "name");

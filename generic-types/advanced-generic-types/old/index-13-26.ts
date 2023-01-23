b858ed7187f750b55741a4456697f2ab3cb968d8
Sergei-Nikiforov <pclp@inbox.ru>
2023-01-23 22:25:50 +0300

    advanced generic types

---
+++ generic-types/advanced-generic-types/old/index-13-26.ts
@@ -0,0 +1,17 @@
+import { City, Person, Product, Employee} from "../src/dataTypes";
+
+type Filter<T, U> = T extends U ? never : T;
+
+function FilterArray<T, U>(data: T[],
+    predicate: (item: any) => item is U): Filter<T, U>[] {
+        return data.filter(item => !predicate(item)) as any;
+}
+
+let dataArray = [new Product("Kayak", 275), new Person("Bob", "London"), new Product("Lifejacket", 27.50)];
+
+function isProduct(item: any): item is Product {
+    return item instanceof Product;
+}
+
+let filteredData: Person[] = FilterArray(dataArray, isProduct);
+filteredData.forEach(item => console.log(`Person: ${item.name}`));
